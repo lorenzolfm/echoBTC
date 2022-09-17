26466937ce81e3cb5ef696aed26f845f5daa9d86
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-09-17 09:34:50 -0300

    feat: retweets tweets from Tweet array

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use dotenv::dotenv;
 use reqwest::{blocking::Client, header::AUTHORIZATION};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 const BASE_URL: &str = "https://api.twitter.com/2";
 const GET_RECENT_TWEETS: &str = "/tweets/search/recent";
+const POST_RETWEET: &str = "/users/2597897487/retweets";
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Response {
@@ -17,13 +19,6 @@ struct Tweet {
     author_id: String,
 }
 
-fn get_bearer_token_auth_header() -> String {
-    let bearer_token = std::env::var("BEARER_TOKEN")
-        .expect("BEARER_TOKEN environment variable must be set");
-
-    format!("Bearer {}", bearer_token)
-}
-
 fn get_url(endpoint: &str) -> String {
     let mut url = BASE_URL.to_string();
     url.push_str(endpoint);
@@ -34,10 +29,7 @@ fn get_url(endpoint: &str) -> String {
 fn get_tweets(client: &Client) -> Vec<Tweet> {
     let url = get_url(GET_RECENT_TWEETS);
     let authorization_header = get_bearer_token_auth_header();
-    let query = [
-        ("query", "@echoBTC"),
-        ("expansions", "author_id")
-    ];
+    let query = [("query", "@echoBTC"), ("expansions", "author_id")];
 
     client
         .get(url)
@@ -50,12 +42,55 @@ fn get_tweets(client: &Client) -> Vec<Tweet> {
         .data
 }
 
+fn get_bearer_token_auth_header() -> String {
+    let bearer_token =
+        std::env::var("BEARER_TOKEN").expect("BEARER_TOKEN environment variable must be set");
+
+    format!("Bearer {}", bearer_token)
+}
+
+fn post_retweet(client: &Client, tweet: &Tweet) {
+    let url = get_url(POST_RETWEET);
+    let authorization_header = get_oauth1_header(&url);
+    let body = HashMap::from([
+        ("tweet_id", &tweet.id)
+    ]);
+
+    let res = client
+        .post(url)
+        .header(AUTHORIZATION, authorization_header)
+        .json(&body)
+        .send()
+        .unwrap();
+
+    println!("{:#?}", res.text());
+}
+
+fn get_oauth1_header(url: &String) -> String {
+    let consumer_key = std::env::var("API_KEY")
+        .expect("API_KEY environment variable must be set.");
+    let consumer_secret = std::env::var("API_SECRET_KEY")
+        .expect("API_SECRET_KEY environment variable must be set.");
+    let access_token = std::env::var("ACCESS_TOKEN")
+        .expect("ACCESS_TOKEN environment variable must be set.");
+    let token_secret = std::env::var("ACCESS_TOKEN_SECRET")
+        .expect("ACCESS_TOKEN_SECRET environment variable must be set.");
+
+    let token = oauth::Token::from_parts(consumer_key, consumer_secret, access_token, token_secret);
+
+    oauth::post(url, &(), &token, oauth::HMAC_SHA1)
+}
+
 fn main() -> Result<(), reqwest::Error> {
     dotenv().ok();
 
     let client = Client::new();
+    let tweets = get_tweets(&client);
 
-    println!("{:#?}", get_tweets(&client));
+    for i in 0..tweets.len() {
+        post_retweet(&client, &tweets[i]);
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
 
     Ok(())
 }
